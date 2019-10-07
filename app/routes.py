5f34738f85343bd5ecccd6b5d9556dfdaf70048d
shumyk <dima.shumyk@gmail.com>
2019-10-07 23:07:21 +0300

    reformated app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,13 @@ from app import app
 from app.forms import UploadForm
 from app.files.file_worker import FileWorker
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Vania Maksymiv'}
     return render_template('index.html', title='Upload your files', user=user)
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -17,6 +19,8 @@ def upload():
         FileWorker.save_upload_files(request.files)
 
         flash('Uploaded files: template - {}, names - {}{}'.format(
-            form.badge_template.name, form.names_excel.name, ', font - ' + form.font.name if form.font is None else ''))
+            form.badge_template.name,
+            form.names_excel.name,
+            ', font - ' + form.font.name if form.font is None else ''))
         return redirect(url_for('index'))
     return render_template('upload.html', title='Upload your files', form=form)
