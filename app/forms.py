c3b84a09f4ce2cc819b622bbc99d673c1f6a8467
shumyk <dima.shumyk@gmail.com>
2019-10-07 23:36:43 +0300

    added additional new lines in files in order to follow convention

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class UploadForm(FlaskForm):
     badge_template = FileField('Badge template', validators=[DataRequired()])
     names_excel = FileField('Names', validators=[DataRequired()])
