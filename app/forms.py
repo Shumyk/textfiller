be1e6e6c1c3a7023c7fc110def4d550f4c06f410
shumyk <dima.shumyk@gmail.com>
2019-10-07 16:49:07 +0300

    created upload page, updated base.html with flashed messages, added config setting

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
+
+class UploadForm(FlaskForm):
+    badge_template = FileField('Badge template', validators=[DataRequired()])
+    names_excel = FileField('Names', validators=[DataRequired()])
+    font = FileField('Font (optional)')
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import UploadForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Vania Maksymiv'}
     return render_template('index.html', title='Upload your files', user=user)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        flash('Uploaded files: template - {}, names - {}{}'.format(
+            form.badge_template.name, form.names_excel.name, ', font - ' + form.font.name if form.font is None else ''))
+        return redirect(url_for('index'))
+    return render_template('upload.html', title='Upload your files', form=form)
